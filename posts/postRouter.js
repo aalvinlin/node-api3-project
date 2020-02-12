2985d0ed67b3a51d18a519302cd54cf931891f5c
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2020-02-12 12:21:57 -0800

    GET /api/posts working

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,20 +1,29 @@
 const express = require('express');
+const database = require("./postDb");
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
+  database.get()
+    .then(response => {
+      console.log("database response:", response);
+      res.status(200).json(response);
+    })
+    .catch (error => {
+      console.log("database error:", error)
+      res.status(500).json({ message: "database error: GET /" });
+    })
+});
+
+router.get('/:id', validatePostId, (req, res) => {
   // do your magic!
 });
 
-router.get('/:id', (req, res) => {
+router.delete('/:id', validatePostId, (req, res) => {
   // do your magic!
 });
 
-router.delete('/:id', (req, res) => {
-  // do your magic!
-});
-
-router.put('/:id', (req, res) => {
+router.put('/:id', validatePostId, (req, res) => {
   // do your magic!
 });
 
