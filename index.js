ffa4257f7691980df77f9c3081864ce21a015fdd
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2020-02-12 11:24:30 -0800

    Server running on port 5000 with '/' endpoint working

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// code away!
+const express = require("express");
+
+const postRouter = require("./posts/postRouter");
+const userRouter = require("./users/userRouter");
+
+const server = express();
+
+server.use(express.json());
+
+server.use("/api/posts", postRouter);
+server.use("/api/users", userRouter);
+
+server.get("/", (req, res) => {
+    res.send("<h1>User and Post API</h1>");
+})
+
+const port = 5000;
+server.listen(port, () => {
+    console.log("\r\n Server running on http://localhost:" + port + "\r\n");
+})
